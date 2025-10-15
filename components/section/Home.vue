ea48e70f913b1855744806b209361eb601758187
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-10-15 13:32:21 +0800

    fix motto text centering

--- components/section/Home.vue
+++ components/section/Home.vue
@@ -22,16 +22,11 @@
           Hubungi Kami
         </WidgetsButtonBaseButton>
       </div>
-      <div class="pr-9 sm:pr-10 md:pr-[14.6rem]">
-        <!-- <img
-          src="/public/image/Elektronika tidak memenangkan pertempuran.svg"
-          alt=""
-        /> -->
-        <h1 class="font-semibold text-center">
-          MENDOMINASI RUANG SPEKTRUM ELEKTROMAGNETIK MERUPAKAN INTI
-          KEKUATAN PUSKOMLEKAD
-        </h1>
-      </div>
+
+      <h1 class="font-semibold text-center">
+        MENDOMINASI RUANG SPEKTRUM ELEKTROMAGNETIK MERUPAKAN INTI
+        KEKUATAN PUSKOMLEKAD
+      </h1>
     </div>
   </div>
 </template>
